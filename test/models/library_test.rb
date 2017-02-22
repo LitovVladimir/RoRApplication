f85f94e9359b8f1bdcc23214ff6554b443bd0b38
Владимир Литов <vladimirlitov@Vladimirs-Mac.local>
2017-02-23 01:18:46 +0300

    Добавлены тесты для модели library

--- test/models/library_test.rb
+++ test/models/library_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class LibraryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "create model without number" do
+     Library.create(number: nil, name: "Тестовая библиотека", address: "Зеленоград ул Юности дом 1")
+     assert false if Library.where(:name => 'Тестовая библиотека').exists?
+   end
+end
+
+class LibraryTest < ActiveSupport::TestCase
+  test "create model without name" do
+    Library.create(number: 42, name: "", address: "Зеленоград ул Юности дом 1")
+    assert false if Library.where(:number => 42).exists?
+  end
+end
+
+class LibraryTest < ActiveSupport::TestCase
+  test "create model without address" do
+    Library.create(number: 42, name: "Тестовая библиотека", address: "")
+    assert false if Library.where(:name => 'Тестовая библиотека').exists?
+  end
 end
